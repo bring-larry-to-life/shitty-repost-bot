45cadb35d4705be0e9ecfa3f40c9c167f05e18c5
Ryan Stanley <stanleyrya@gmail.com>
2016-06-13 01:55:19 -0400

    every 3 hours

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var jobs = require('./lib/jobs');
 var reddit = require('./lib/reddit');
 
 // hourly
-jobs.createJob('0 * * * *', function() {
+jobs.createJob('0 */3 * * *', function() {
 	reddit.invokeRepost();
 });
