0c2d0a25de5bc76cfaada4ef75a9939a328583dd
Ryan Stanley <stanleyrya@gmail.com>
2016-06-14 22:46:39 -0400

    Revert "back to every three hours"
    This reverts commit c1c40daf980979646f5605cbfca7376a3fb1c303.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var jobs = require('./lib/jobs');
 var reddit = require('./lib/reddit');
 
 // every three hours
-jobs.createJob('0 */3 * * *', function() {
+jobs.createJob('44 * * * *', function() {
 	console.log('Job executing at: ' + new Date().toString());
 	reddit.invokeRepost();
 });
