003b54ed08a817a7e268c4f820cb0b1320194a05
Ryan Stanley <stanleyrya@gmail.com>
2016-06-17 04:28:22 -0400

    moved logging logic back to reddit.js in the form of a default callback

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var jobs = require('./lib/jobs');
 var reddit = require('./lib/reddit');
-var logger = require('./lib/logger');
 
 // required on raspberry pi to fix setInverval() problem
 // https://github.com/nodejs/node/issues/4262
@@ -8,11 +7,5 @@ jobs.createJob('*/20 * * * *', function() {});
 
 // repost every three hours
 jobs.createJob('0 */3 * * *', function() {
-	reddit.invokeRepost(undefined, function(error, postData) {
-		if (error) {
-			logger.error(error, -1, logger.LOG_TYPE.REPOST);
-		} else {
-			logger.log(postData, logger.LOG_TYPE.REPOST);
-		}
-	});
+	reddit.invokeRepost();
 });
